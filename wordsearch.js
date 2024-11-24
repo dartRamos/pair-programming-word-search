81f64e487cf7e9719fb9241358ea2b6ede1da934
Dartanhan Ramos <alessandroramosd@gmail.com>
2024-11-23 19:07:42 -0500

    Addded a vertical search check to see if words exists in the columns.'

--- wordsearch.js
+++ wordsearch.js
@@ -3,6 +3,18 @@ const wordSearch = (letters, word) => {
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
-}
+
+    for (let col = 0; col < letters[0].length; col++) { // loops through first array of letters
+        let verticalWord = '';
+        for (let row = 0; row < letters.length; row ++) { // loops through each row
+        verticalWord += letters[row][col]; // adds the letter from the current row and current col into verticalWord string.
+        }
+    
+        if (verticalWord.includes(word)) { // if the letters gathered from loop form the word that was inputted
+            return true;
+        }
+    }
+    return false;
+};
 
 module.exports = wordSearch
